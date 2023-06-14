d1ce6127ef9e38a0e23284f02bc807426a38d297
rodrigomesquita-dev <rodrigomesquita9232@gmail.com>
2023-06-13 22:39:42 -0400

    first commit

---
+++ src/components/layouts/Conteudo.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <h1>Conteudo</h1>
+        <!--Renderizar de modo dinâmico, home e publicar vaga-->
+        <button @click="conteudo = 'home'" >Home</button>
+        <button @click="conteudo = 'publicar-vaga'" >Publicar Vaga</button>
+        <keep-alive>
+            <component :is="conteudo"/>
+        </keep-alive>
+    </div>
+</template>
+
+<script>
+    import Home from '../views/Home.vue'
+    import PublicarVaga from '../views/PublicarVaga.vue'
+    
+    export default {
+        name: 'Conteudo',
+        components: {
+            Home,
+            PublicarVaga,
+        },
+        data: () => ({
+            conteudo: 'home',
+        }),
+    }
+</script>
+
+<style scoped>
+    .teste1 {
+        background: blue;
+        color: white;
+    }
+</style>
---
+++ src/components/views/Home.vue
@@ -0,0 +1,63 @@
+<template>
+    <div class="container py-4">
+        <div class="row">
+            <div class="col">
+                <div class="h-100 p-5 bg-light border rounded-3">
+                    <h2>Pesquisar vaga</h2>
+                    <div class="row">
+                        <div class="col">
+                            <div class="form-group">
+                                <label>Título da Vaga</label>
+                                <input type="text" class="form-control" placeholder="Pesquise por palavras chaves"/>
+                                <small class="form-text text-muted">Informe palavras relacionada com o título da vaga</small>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col">
+                            <button class="btn btn-outline-dark mt-2" type="button">Buscar</button>
+                        </div>
+                    </div>
+                    
+                </div>
+            </div>
+        </div>
+        <div class="row mt-5">
+            <div class="col-4">
+                <div class="h-100 p-5 rounded-3 border bg-dark text-white">
+                    <p>Vagas abertas</p>
+                    <h2>25</h2>
+                </div>
+            </div>
+
+            <div class="col-4">
+                <div class="h-100 p-5 rounded-3 border bg-dark text-white">
+                    <p>Profissionais Cadastrados</p>
+                    <h2>125</h2>
+                </div>
+            </div>
+
+            <div class="col-4">
+                <div class="h-100 p-5 rounded-3 border bg-light text-dark">
+                    <p>Visitantes online</p>
+                    <h2>15</h2>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Home',
+        unmounted() {
+            console.log('desmontado')
+        }
+    }
+</script>
+
+<style scoped>
+    h1 {
+        color: blue
+    }
+</style>
